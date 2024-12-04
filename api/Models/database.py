a72a2e2102650f1f5eedd1dca51e0b73e2285e3e
Edwin Oswaldo Torres Rincón <edwin.torres.r2000@gmail.com>
2024-12-03 22:42:01 -0500

    Se actualiza el database.py

--- api/Models/database.py
+++ api/Models/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "mysql+mysqlconnector://root:fuwbYNrLduLeLciYpeMtjdbPmPMkAzoc@autorack.proxy.rlwy.net:40107/railway"
+DB_USER = "root"
+DB_PASSWORD = "fuwbYNrLduLeLciYpeMtjdbPmPMkAzoc"
+DB_HOST = "autorack.proxy.rlwy.net"
+DB_PORT = "40107"
+DB_NAME = "railway"
 
-engine = create_engine(DB_URL)
+engine = create_engine(f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
